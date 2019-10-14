cb6e43c148a0a15a5d54a8e83c54683f0766a04f
AirryYuan <xzyuan.cn@qq.com>
2019-10-14 11:33:07 +0800

    更新外窗数据结构

--- SunacCoordination/SunacCoordination/Object/WindowDoor/AttrWindow.cpp
+++ SunacCoordination/SunacCoordination/Object/WindowDoor/AttrWindow.cpp
@@ -1,12 +1,92 @@
-/////////////////////////////////////////////
-// ZffDwgScale custom object.
 #include "StdAfx.h"
 #include "dbproxy.h"
 #include "geassign.h"
 #include "acgi.h"
 #include "AttrWindow.h"
 #include "../../Common/ComFun_Sunac.h"
+#include "../../Common/ComFun_Math.h"
 
+eWindowDimType ToEWindowType(CString type)
+{
+	if (type == "固定值")
+	{
+		return SINGLE;
+	}
+	else if (type == "值系列")
+	{
+		return MULTI;
+	}
+	else if (type == "范围")
+	{
+		return SCOPE;
+	}
+	else if (type == "不限")
+	{
+		return UNLIMIT;
+	}
+	else if (type == "公式")
+	{
+		return CALC;
+	}
+	else if (type == "无")
+	{
+		return NOVALUE;
+	}
+	else
+	{
+		ASSERT(FALSE);
+		return NOVALUE;
+	}
+}
+CWindowsDimData::CWindowsDimData()
+{
+	type = NOVALUE;
+	minValue = 0;
+	maxValue = 10000;
+	defaultValue = 0;
+	value = 0;
+}
+
+bool CWindowsDimData::operator==(const CWindowsDimData &rhs) const
+{
+	if (sCodeName != rhs.sCodeName || type != rhs.type)
+	{
+		return false;
+	}
+
+	switch (type)
+	{
+	case SINGLE:
+	case MULTI:
+		return valueOptions == rhs.valueOptions;
+		break;
+	case SCOPE:
+		return JHCOM_equ(minValue, rhs.minValue, 0.1) &&
+			JHCOM_equ(maxValue, rhs.maxValue, 0.1);
+		break;
+	case CALC:
+		return sFomula == rhs.sFomula;
+		break;
+	default:
+		break;
+	}
+
+	return true;
+}
+
+bool CWindowsDimData::IsValueEqual(const CWindowsDimData &rhs)const
+{
+	if (type==NOVALUE)
+	{
+		return true;
+	}
+	else
+	{
+		return JHCOM_equ(value, rhs.value, 0.1);
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////
 
 //{{AFX_ARX_MACRO
 ACRX_DXF_DEFINE_MEMBERS(AttrWindow, AcDbObject,
@@ -20,19 +100,13 @@ AttrWindow::AttrWindow()
 	m_isZhuanJiao = false;//是否转角窗
 	m_isMirrorWindow = false;
 
-	m_minWid = 0;//宽度尺寸最小值
-	m_maxWid = 0;//宽度尺寸最大值
 	m_tongFengFormula = L"";//通风量计算公式
-	m_staticTongFengQty = 0.0;
-	m_staticWidth = 0;
-	m_staticHeight = 0;
+	m_tongFengQty = 0.0;
 
 	//////////////////////////////////////////////////////////////////////////
 	m_isMirror = false;
 	m_viewDir = E_VIEW_FRONT;
 	m_isBayWindow = false;
-	m_openWindowSize = 0;
-	m_windowH2 = 0;
 	m_plugslotSize = 0;
 	m_wallDis = 0.0;
 }
@@ -57,7 +131,7 @@ AttrWindow::~AttrWindow()
 //	m_minWid = other.m_minWid;
 //	m_maxWid = other.m_maxWid;
 //	m_tongFengFormula = other.m_tongFengFormula;
-//	m_staticTongFengQty = other.m_staticTongFengQty;
+//	m_tongFengQty = other.m_tongFengQty;
 //
 //	m_isMirror = other.m_isMirror;
 //	m_viewDir = other.m_viewDir;
@@ -77,7 +151,7 @@ AttrWindow::~AttrWindow()
 //	m_tongFengFormula = other.m_tongFengFormula;	//通风量公式	
 //	m_width = other.m_width;		//静态宽度	
 //	m_height = other.m_height;		//静态高度	
-//	m_staticTongFengQty = other.m_staticTongFengQty;	//静态通风量	
+//	m_tongFengQty = other.m_tongFengQty;	//静态通风量	
 //	m_topViewFile = other.m_topViewFile;	//原型平面文件
 //	m_leftViewFile = other.m_leftViewFile;//原型俯视图文件
 //
@@ -85,6 +159,33 @@ AttrWindow::~AttrWindow()
 //
 //	return *this;
 //}
+
+const CWindowsDimData* AttrWindow::GetDimData(CString p_sCode)const
+{
+	for (UINT i = 0; i < m_dimData.size(); i++)
+	{
+		if (m_dimData[i].sCodeName.CompareNoCase(p_sCode) == 0)
+		{
+			return &(m_dimData[i]);
+		}
+	}
+
+	assert(false);
+	return NULL;
+}
+void AttrWindow::SetDimData(const CWindowsDimData& p_dim)
+{
+	for (UINT i = 0; i < m_dimData.size(); i++)
+	{
+		if (m_dimData[i].sCodeName.CompareNoCase(p_dim.sCodeName)==0)
+		{
+			m_dimData[i] = p_dim;
+			return ;
+		}
+	}
+
+	m_dimData.push_back(p_dim);
+}
 
 Acad::ErrorStatus AttrWindow::dwgInFields(AcDbDwgFiler* filer)
 {
@@ -100,13 +201,11 @@ Acad::ErrorStatus AttrWindow::dwgInFields(AcDbDwgFiler* filer)
 
 	filer->readItem(&m_openQty);
 	filer->readItem(&m_isZhuanJiao);
-	filer->readItem(&m_minWid);
-	filer->readItem(&m_maxWid);
 
 	filer->readItem(&tempStr);
 	m_tongFengFormula = CString(tempStr);
 
-	filer->readItem(&m_staticTongFengQty);
+	filer->readItem(&m_tongFengQty);
 
 
 	//TODO 把所有的属性读取补充完整
@@ -130,10 +229,8 @@ Acad::ErrorStatus AttrWindow::dwgOutFields(AcDbDwgFiler* filer) const
 	filer->writeItem(m_openType);
 	filer->writeItem(m_openQty);
 	filer->writeItem(m_isZhuanJiao);
-	filer->writeItem(m_minWid);
-	filer->writeItem(m_maxWid);
 	filer->writeItem(m_tongFengFormula);
-	filer->writeItem(m_staticTongFengQty);
+	filer->writeItem(m_tongFengQty);
 
 	//TODO 把所有的属性读取补充完整
 
@@ -157,10 +254,8 @@ bool AttrWindow::isEqualTo(AttrObject*other)
 	return (m_openType == pRealObj->m_openType &&
 		m_openQty == pRealObj->m_openQty &&
 		m_isZhuanJiao == pRealObj->m_isZhuanJiao &&
-		m_minWid == pRealObj->m_minWid &&
-		m_maxWid == pRealObj->m_maxWid &&
 		m_tongFengFormula == pRealObj->m_tongFengFormula &&
-		m_staticTongFengQty == pRealObj->m_staticTongFengQty &&
+		m_tongFengQty == pRealObj->m_tongFengQty &&
 		m_isMirror == pRealObj->m_isMirror &&
 		m_viewDir == pRealObj->m_viewDir &&
 		m_wallDis == pRealObj->m_wallDis
@@ -171,42 +266,15 @@ bool AttrWindow::IsPrototypeEqual(const AttrWindow& p_att)
 {
 	if (p_att.m_dimData.empty()||m_dimData.empty())
 	{
-		auto it1 = p_att.m_dimData.begin();
-		auto it2 = m_dimData.begin();
+		vector<CWindowsDimData>::const_iterator it1 = p_att.m_dimData.begin();
+		vector<CWindowsDimData>::iterator it2 = m_dimData.begin();
 		while (it1 != p_att.m_dimData.end()&& it2 != m_dimData.end())
 		{
-			if (it1->values != it2->values)
+			if ((*it1)==(*it2)==false)
 			{
 				return false;
 			}
-			if (it1->type != it2->type)
-			{
-				return false;
-			}
-			if (it1->sFomula != it2->sFomula)
-			{
-				return false;
-			}
-			if (it1->sCodeName != it2->sCodeName)
-			{
-				return false;
-			}
-			if (it1->prompt != it2->prompt)
-			{
-				return false;
-			}
-			if (it1->minValue != it2->minValue)
-			{
-				return false;
-			}
-			if (it1->maxValue != it2->maxValue)
-			{
-				return false;
-			}
-			if (it1->defaultValue != it2->defaultValue)
-			{
-				return false;
-			}
+
 			it1++;
 			it2++;
 		}
@@ -263,23 +331,11 @@ bool AttrWindow::IsPrototypeEqual(const AttrWindow& p_att)
 	{
 		return false;
 	}
-	else if (p_att.m_maxWid != m_maxWid)
-	{
-		return false;
-	}
-	else if (p_att.m_minWid != m_minWid)
-	{
-		return false;
-	}
 	else if (p_att.m_openQty != m_openQty)
 	{
 		return false;
 	}
 	else if (p_att.m_openType != m_openType)
-	{
-		return false;
-	}
-	else if (p_att.m_openWindowSize != m_openWindowSize)
 	{
 		return false;
 	}
@@ -299,15 +355,7 @@ bool AttrWindow::IsPrototypeEqual(const AttrWindow& p_att)
 	{
 		return false;
 	}
-	else if (p_att.m_staticHeight != m_staticHeight)
-	{
-		return false;
-	}
-	else if (p_att.m_staticTongFengQty != m_staticTongFengQty)
-	{
-		return false;
-	}
-	else if (p_att.m_staticWidth != m_staticWidth)
+	else if (p_att.m_tongFengQty != m_tongFengQty)
 	{
 		return false;
 	}
@@ -332,10 +380,6 @@ bool AttrWindow::IsPrototypeEqual(const AttrWindow& p_att)
 		return false;
 	}
 	else if (p_att.m_wallDis != m_wallDis)
-	{
-		return false;
-	}
-	else if (p_att.m_windowH2 != m_windowH2)
 	{
 		return false;
 	}
