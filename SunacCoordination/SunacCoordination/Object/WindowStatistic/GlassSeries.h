e17e3053017b7f61331bfb83182d7da8cd35bdea
AirryYuan <xzyuan.cn@qq.com>
2019-10-27 10:43:58 +0800

    新增附框和玻璃信息表

---
+++ SunacCoordination/SunacCoordination/Object/WindowStatistic/GlassSeries.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+
+class CGlassData
+{
+public:
+	CGlassData();
+	~CGlassData();
+
+	double m_heatInsulationCoefficient; //隔热系数
+	double m_shadingCoefficient; //遮阳系数
+
+	CString m_sSerailName; //玻璃系列名称
+
+	int m_glassCount;  //玻璃层数
+
+	vector<CString> m_alSerials; //匹配的型材
+};
+
+class CGlassSeries
+{
+	CGlassSeries();
+public:
+	static CGlassSeries* Instance();
+	virtual ~CGlassSeries();
+
+	static vector<CString> GetAllGlassSerials();
+
+	////通过型材系列获取玻璃选型
+	//static vector<CString> GetGlassSerialsByAlSerial(CString p_sAlSerial);
+
+	////通过隔热系数获取玻璃选型
+	//static vector<CString> GetGlassSerialsByHeatInsulationCoeff(double p_heatCoeff)
+
+
+
+};
