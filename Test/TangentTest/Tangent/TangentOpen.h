e9125135d6bdf84c899a365cfa231f10b675d112
Airryyuan <xzyuan.cn@qq.com>
2020-03-27 17:21:51 +0800

    使用模板函数实现多个类型

--- Test/TangentTest/Tangent/TangentOpen.h
+++ Test/TangentTest/Tangent/TangentOpen.h
@@ -17,7 +17,7 @@ public:
 	static HRESULT InsertWinOpenning(AcGePoint3d p_centerPt, CTOpenData p_winData, AcDbObjectId &p_tWinOpenIdOut);
 
 protected:
-	static HRESULT SetTangentOpenProp_TCH10_COM9_T20V5X64( AcDbObjectId p_winId, CTOpenData p_winData);
+	//static HRESULT SetTangentOpenProp_TCH10_COM9_T20V5X64( AcDbObjectId p_winId, CTOpenData p_winData);
 
 };
 
